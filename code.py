126eec0858cc1191949d13dd8daac3ddad5547ba
srushtidj <1907srushti@gmail.com>
2023-03-04 23:59:20 +0530

    changed parameters

--- code.py
+++ code.py
@@ -6,12 +6,13 @@ import plotly.express as px
 st.write('# Water Quality dashboard')
 st.markdown(''' This is a dashboard showing the water quality of the given sample.''')
 
-temp_input = st.number_input("Temperature (in degree celcius)", step=0.5)
+temp_input = st.number_input("Temperature (°C))", step=0.5)
 pH_input = st.number_input("pH", step=0.5)
-sal_input = st.number_input("Salinity (dS/m)", step=0.5)
-nitro_input = st.number_input("Nitrogen content (mg/L)", step=0.5)
-ars_input = st.number_input("Arsenic content (mg/L)", step=0.5)
+do_input = st.number_input("Dissolved Oxygen (mg/L))", step=0.5)
+tss_input = st.number_input("Total Suspended Solids (mg/L))", step=0.5)
+cond_input = st.number_input("Conductivity (mg/L))", step=0.5)
+nit_input = st.number_input("Nitrate (µS/L))", step=0.5)
 
-wqi = int(temp_input+pH_input+sal_input+nitro_input+ars_input)/5
+wqi = int(temp_input+pH_input+tss_input+cond_input+nit_input)/6
 
 st.header(f'WQI-water quality index: {wqi}')

19a325504306fe6bef294236e9842237349faf73
srushtidj <1907srushti@gmail.com>
2023-04-18 15:41:59 +0530

    changed parameters

--- code.py
+++ code.py
@@ -8,33 +8,38 @@ st.markdown(''' This is a dashboard showing the water quality of the given sampl
 
 temp_input = st.number_input("Temperature (°C)", step=0.5)
 pH_input = st.number_input("pH", step=0.5)
-do_input = st.number_input("Dissolved Oxygen (mg/L)", step=0.5)
-# tss_input = st.number_input("Total Suspended Solids (mg/L)", step=0.5)
-cond_input = st.number_input("Conductivity (µS/cm)", step=0.5)
-nit_input = st.number_input("Nitrate (mg/L)", step=0.5)
+bod_input = st.number_input("Biochemical Oxygen Demand (mg/L)", step=0.5)
+cod_input = st.number_input("Chemical Oxygen Demand (mg/L)", step=0.5)
+tss_input = st.number_input("Total Suspended Solids (mg/L)", step=0.5)
+fc_input = st.number_input("Fecal Coliform (MPN*10^7)", step=0.5)
+# nit_input = st.number_input("Nitrate (mg/L)", step=0.5)
 
-t = abs(temp_input-26.0125)
-p = abs(pH_input-7.3125)
-d = abs(do_input-9)
-c = abs(cond_input-1231.25)
-n = abs(nit_input-4.175)
+temp = abs(temp_input-26.0125)
+pH = abs(pH_input-7.3125)
+bod = abs(bod_input-9)
+cod = abs(cod_input-1231.25)
+tss = abs(tss_input-4.175)
+fc = abs(fc_input)
 
-sub_temp = abs(100- (t*100/(30-20)))
-sub_pH = abs(100 - (p*100/(8.5-6.5)))
-sub_do = abs(100 - (d*100/14))
-sub_cond = abs(100 - (c*100/(2000-700)))
-sub_nit = abs(100 - (n*100/10))
 
-wt = 0.055
-wp = 0.358
-wd = 0.068
-wc = 0.295
-wn = 0.224
+sub_temp = abs(100- (temp*100/(30-20)))
+sub_pH = abs(100 - (pH*100/(8.5-6.5)))
+sub_bod = abs(100 - (bod*100/14))
+sub_cod = abs(100 - (cod*100/(2000-700)))
+sub_tss = abs(100 - (tss*100/10))
+# sub_fc = abs()
 
-if temp_input==pH_input==do_input==cond_input==nit_input == 0:
+w_temp = 0.055
+w_pH = 0.358
+w_bod = 0.068
+w_cod = 0.295
+w_tss = 0.224
+# w_fc = 
+
+if temp_input==pH_input==bod_input==cod_input==tss_input == 0:
     wqi = 0
 else:
-    wqi = ((sub_temp**wt)*(sub_pH**wp)*(sub_do**wd)*(sub_cond**wc)*(sub_nit**wn))
+    wqi = ((sub_temp**w_temp)*(sub_pH**w_pH)*(sub_bod**w_bod)*(sub_cod**w_cod)*(sub_tss**w_tss))
     wqi = round(wqi,2)
 
 st.header(f'WQI-water quality index: {wqi}')
